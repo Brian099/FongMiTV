9ee754ddc5cd6edb31735a42a53236c1bb826a83
Brian099 <luohuafengchuiqi@163.com>
2026-01-06 10:38:52 +0800

    修改更新地址

--- app/src/main/java/com/fongmi/android/tv/utils/Github.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Github.java
@@ -2,7 +2,8 @@ package com.fongmi.android.tv.utils;
 
 public class Github {
 
-    public static final String URL = "https://raw.githubusercontent.com/FongMi/Release/fongmi";
+    // public static final String URL = "https://raw.githubusercontent.com/FongMi/Release/fongmi";
+	public static final String URL = "https://tvboxadmin.com:2026";
 
     private static String getUrl(String name) {
         return URL + "/apk/" + name;
